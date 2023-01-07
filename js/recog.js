070cca62f3bb78ac7d94d0bd21cde967efa0dd03
miktae <86813820+miktae@users.noreply.github.com>
2023-01-07 21:26:57 +0700

    OD

--- js/recog.js
+++ js/recog.js
@@ -6,12 +6,12 @@ import { storage } from './firebase.js'
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.ageGenderNet.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
+    faceapi.nets.ageGenderNet.loadFromUri('../models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('../models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('../models')
 ]).then(startVideo)
 
 function startVideo() {
